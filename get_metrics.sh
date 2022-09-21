14e669092042d015a7eeab55c246ced5801d89fd
Moacir_MSJ <moacir.mascarenhas@vamosparcelar.com.br>
2022-09-21 00:30:03 -0300

    coleta front end

--- get_metrics.sh
+++ get_metrics.sh
@@ -4,12 +4,15 @@
 # Para executar passe a lista de PRs analisados
 # Exemplo: bash get_metrics.sh 1 2 3 4
 
+# PROJECT_KEY=fga-eps-mds-2022-1-Alectrion-FrontEnd
 
-PROJECT_KEY=
+# PROJECT_KEY=fga-eps-mds-2022-1-Alectrion-UserAPI
+# PROJECT_KEY=fga-eps-mds-2022-1-Alectrion-EquipamentApi
 
+# PROJECT_KEY=fga-eps-mds-2022-1-Alectrion-FrontEnd
 for pr in "$@" 
 do  
     NOW=$(date +"%m-%d-%Y-%H-%M-%S");
     echo "PR NUMBER: $pr";
-    curl "https://sonarcloud.io/api/measures/component_tree?additionalFields=metrics&component=$PROJECT_KEY&metricKeys=files,functions,complexity,comment_lines_density,duplicated_lines_density,coverage,ncloc,tests,test_errors,test_failures,test_execution_time,security_rating&pullRequest=$pr&qualifiers=FIL,TRK,UTS,DIR&strategy=all"> analytics-raw-data/$PROJECT_KEY-$NOW-pr$pr.json
+    curl http://localhost:9000/api/measures/component_tree?component=fga-eps-mds-2022-1-Alectrion-FrontEnd&metricKeys=files,functions,complexity,comment_lines_density,duplicated_lines_density,coverage,ncloc,tests,test_errors,test_failures,test_execution_time,security_rating&ps=500&p=1 > analytics-raw-data/fga-eps-mds-2022-1-Alectrion-FrontEnd-$NOW-v0.$pr.0.json
 done
